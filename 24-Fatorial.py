9e269a7cb3d2692f9aed31bcd76c13362612c04f
DiogoEngh <97733618+DiogoEngh@users.noreply.github.com>
2022-05-05 00:12:27 -0300

    Update 24-Fatorial.py

--- 24-Fatorial.py
+++ 24-Fatorial.py
@@ -1,14 +1,14 @@
-def fib(numero):
+def fatorial(numero):
   if numero == 0:
     return 1
   if numero == 1:
     return 1
   else:
-    return numero * fib(numero-1)
+    return numero * fatorial(numero-1)
   
 while True:
   numero = int(input())
   if numero == -1:
     break
   else:
-    print(fib(numero))
+    print(fatorial(numero))
